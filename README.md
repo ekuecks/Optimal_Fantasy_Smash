6f511d4ad28095082a82e822b6a3960a48c42b9f
Eric Kuecks <ekuecks@Erics-MacBook-Pro.local>
2016-01-17 10:56:23 -0800

    uploading all files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Optimal_Fantasy_Smash
+Edit the players.txt file with the results of the tournament with the format
+salary_cap
+place name salary score
+place name salary score
+...
+
+See the sample file for Genesis 3 (top 8 currently projected)
+
+Then run `python optimal.py` to see the optimal lineup for that tournament
---
+++ optimal.py
@@ -0,0 +1,64 @@
+import copy
+# dp array nxWx13
+dp = []
+
+# players -> array of all players as a tuple of (name, salary, value)
+# players_left -> roster space left
+# cap -> cap room left
+def optimal(players, players_left, cap):
+    num_players = len(players)
+    # base cases
+    # no more players left to look at
+    # or no more room on the roster
+    # or we have already solved this case
+    if dp[num_players][players_left][cap] != None:
+        return copy.deepcopy(dp[num_players][players_left][cap])
+    # either the first player in the player array is in the optimal solution or isn't
+    not_in = optimal(players[1:], players_left, cap)
+    # is there enough slary cap for this player
+    signable = cap - players[0][1] >= 0
+    is_in = None
+    if signable:
+        is_in = optimal(players[1:], players_left - 1, cap - players[0][1])
+    # compare the values and return larger
+    if not signable or not_in[0] > is_in[0] + players[0][2]:
+        dp[num_players][players_left][cap] = not_in
+    else:
+        dp[num_players][players_left][cap] = (is_in[0] + players[0][2], is_in[1] + [players[0]])
+    return dp[num_players][players_left][cap]
+
+def main():
+    ROSTER_SIZE = 12
+    # players.txt holds the file with all players and their value and salary
+    # as well as the salary cap
+    f = open('players.txt', 'r')
+    cap = int(f.readline().strip())
+    # build up the players array
+    players = []
+    for line in f:
+        line = line.strip()
+        vals = line.split(' ')
+        # vals = [place, name, salary, value]
+        players.append((vals[1], int(vals[2]), int(vals[3])))
+    num = len(players)
+    # build up the dynamic programming matrix
+    for i in range(num + 1):
+        dp.append([])
+        for j in range(ROSTER_SIZE + 1):
+            dp[i].append([])
+            for k in range(cap + 1):
+                dp[i][j].append(None)
+    # base cases initialization
+    for j in range(ROSTER_SIZE + 1):
+        for k in range(cap + 1):
+            dp[0][j][k] = (0, [])
+    for i in range(num + 1):
+        for k in range(cap + 1):
+            dp[i][0][k] = (0, [])
+    opt = optimal(players, ROSTER_SIZE, cap)
+    for player in opt[1]:
+        print(player[0] + " " + "$" + str(player[1]) + ": " + str(player[2]))
+    print("Optimal Score: " + str(opt[0]))
+
+if __name__ == "__main__":
+    main()
