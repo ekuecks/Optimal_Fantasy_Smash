84498775830825835b593bb991be4642341fc6e5
Eric Kuecks <ekuecks@Erics-MacBook-Pro.local>
2016-01-18 12:53:11 -0800

    Changed the script to require exactly 12 players. Added filler players to the examples to fill our a roster

--- optimal.py
+++ optimal.py
@@ -8,20 +8,32 @@ dp = []
 def optimal(players, players_left, cap):
     num_players = len(players)
     # base cases
-    # no more players left to look at
+    # not enough players left to fill out the roster
+    # or no more players left to look at
     # or no more room on the roster
     # or we have already solved this case
     if dp[num_players][players_left][cap] != None:
         return dp[num_players][players_left][cap]
     # either the first player in the player array is in the optimal solution or isn't
-    not_in = optimal(players[1:], players_left, cap)
+    # if we don't sign this player, will there be enough players left to fill the roster
+    excludable = num_players > players_left
+    not_in = (-1, [])
+    if excludable:
+        not_in = optimal(players[1:], players_left, cap)
+    if not_in[0] < 0:
+        excludable = False
     # is there enough slary cap for this player
     signable = cap - players[0][1] >= 0
-    is_in = None
+    is_in = (-1, [])
     if signable:
         is_in = optimal(players[1:], players_left - 1, cap - players[0][1])
+    if is_in[0] < 0:
+        signable = False
     # compare the values and return larger
-    if not signable or not_in[0] > is_in[0] + players[0][2]:
+    # cannot make a 12-player team with these picks
+    if not excludable and not signable:
+        dp[num_players][players_left][cap] = (-1, [])
+    elif excludable and (not signable or not_in[0] > is_in[0] + players[0][2]):
         dp[num_players][players_left][cap] = not_in
     else:
         dp[num_players][players_left][cap] = (is_in[0] + players[0][2], is_in[1] + [players[0]])
@@ -55,6 +67,10 @@ def main():
     for j in range(ROSTER_SIZE + 1):
         for k in range(cap + 1):
             dp[0][j][k] = (0, [])
+    for i in range(1, ROSTER_SIZE):
+        for j in range(i):
+            for k in range(cap + 1):
+                dp[i][j][k] = (-1, [])
     for i in range(num + 1):
         for k in range(cap + 1):
             dp[i][0][k] = (0, [])
